1ff8dedcd0f845accac71aeefbcb768a8c4edaf8
Jamil Torres <torresjamil008@gmail.com>
2026-01-14 19:13:48 -0500

    ugh

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include <vector> 
 
+#include "config.h"
+
+using namespace std;
+
 int main(){
+    cout << "Hello, World!" << endl;
+    
+    GLFWwindow* window;
+
+    if (!glfwInit()){
+        cout << "GLFW failed to start." << endl;
+        return -1;
+    }
+
+    window = glfwCreateWindow(640, 480, "My Window", NULL, NULL);
 
     return 0;
 }
